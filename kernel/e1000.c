34b0ce2da736a2e93dfc24f9c74e414bb2dbabcd
maple-ysd <1757209040@qq.com>
2021-08-15 23:05:35 +0800

    net lab finished

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,25 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
+  // printf("start sending one packet:\n");
+  acquire(&e1000_lock);
+  // acquire tx_ring index
+  int index = regs[E1000_TDT];
+  // check its status
+  if (!(tx_ring[index].status & E1000_TXD_STAT_DD)) {
+    printf("last transmitting has not been finished.\n");
+    release(&e1000_lock);
+    return -1;
+  } 
+  if (tx_mbufs[index]) 
+    mbuffree(tx_mbufs[index]);
+  tx_mbufs[index] = m;
+  tx_ring[index].addr = (uint64)m->head;
+  tx_ring[index].length = m->len;
+  tx_ring[index].cmd = E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
   
+  regs[E1000_TDT] = (index + 1) % TX_RING_SIZE;
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +133,20 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  // printf("start receiving one packet:\n");
+  acquire(&e1000_lock);
+  int index = (regs[E1000_RDT] + 1) % RX_RING_SIZE;
+  while (rx_ring[index].status & E1000_RXD_STAT_DD) {
+    rx_mbufs[index]->len  = rx_ring[index].length;
+    net_rx(rx_mbufs[index]);
+  
+    rx_mbufs[index] = mbufalloc(0);
+    rx_ring[index].addr = (uint64)rx_mbufs[index]->head;
+    rx_ring[index].status = 0;
+    index = (index + 1) % RX_RING_SIZE;
+  }
+  regs[E1000_RDT] = index - 1;
+  release(&e1000_lock);
 }
 
 void
